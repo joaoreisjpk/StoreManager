03b05df6839803352a52bee314b85038895a5c60
João Reis <joaoreisjpk@gmail.com>
2022-02-01 10:26:35 -0300

    adding more tests

--- test/unit/models.js
+++ test/unit/models.js
@@ -1,114 +1,158 @@
-const sinon = require('sinon');
-const { expect } = require('chai');
+const sinon = require("sinon");
+const { expect } = require("chai");
 
-const connection = require('../../models/connection')
-const productModel = require('../../models/productModel');
+const connection = require("../../models/connection");
+const productModel = require("../../models/productModel");
 
 // Ajuda do Pr do Mannuel Boo
 const product = {
-  correct: { name: 'Primeiro Produto', quantity: 4 },
-  returnCreate: {id: 1, name: 'Primeiro Produto', quantity: 4 },
-  returnGet: {id: 1, name: 'Primeiro Produto', quantity: 4 },
-  returnUptade: {id: 1, name: 'Produto Atualizado', quantity: 400 }
-}
+  correct: { name: "Primeiro Produto", quantity: 4 },
+  returnCreate: { id: 1, name: "Primeiro Produto", quantity: 4 },
+  returnGet: [{ id: 1, name: "Primeiro Produto", quantity: 4 }],
+  returnUptade: { id: 1, name: "Produto Atualizado", quantity: 400 },
+};
 
-describe('Search for a product by their name', () => {
+describe("Search for a product by their name", () => {
   before(async () => {
-    const execute = [[product.returnCreate]];
-    sinon.stub(connection, 'execute').resolves(execute);
+    const execute = [product.returnGet];
+    sinon.stub(connection, "execute").resolves(execute);
   });
   after(async () => {
     connection.execute.restore();
   });
-  describe('when succeed', async () => {
-    it('returns an object', async () => {
+  describe("when succeed", async () => {
+    it("the object isnt empty", async () => {
       const { name } = product.correct;
       const GetByName = await productModel.getByName(name);
 
-      expect(GetByName).to.have.keys('id', 'name', 'quantity')
+      expect(GetByName).to.not.be.empty;
+    });
+    it("have keys", async () => {
+      const { name } = product.correct;
+      const GetByName = await productModel.getByName(name);
+
+      expect(GetByName).to.have.keys("id", "name", "quantity");
+    });
+    it("returns an object", async () => {
+      const { name } = product.correct;
+      const GetByName = await productModel.getByName(name);
+
+      expect(GetByName).to.be.an("object");
     });
   });
 });
 
-describe('Search for a product by their id', () => {
+describe("Search for a product by their id", () => {
   before(async () => {
-    const execute = [[product.returnCreate]];
-    sinon.stub(connection, 'execute').resolves(execute);
+    const execute = [product.returnGet];
+    sinon.stub(connection, "execute").resolves(execute);
   });
   after(async () => {
     connection.execute.restore();
   });
-  describe('when succeed', async () => {
-    it('returns an object', async () => {
-      const GetById = await productModel.getById(1);
+  describe("when succeed", async () => {
+    it("the object isnt empty", async () => {
+      const getById = await productModel.getById(1);
 
-      expect(GetById).to.have.keys('id', 'name', 'quantity')
+      expect(getById).to.not.be.empty;
+    });
+    it("have keys", async () => {
+      const getById = await productModel.getById(1);
+
+      expect(getById).to.have.keys("id", "name", "quantity");
+    });
+    it("returns an object", async () => {
+      const getById = await productModel.getById(1);
+
+      expect(getById).to.be.an("object");
     });
   });
 });
 
-describe('Search for a product list', () => {
+describe("Search for a product list", () => {
   before(async () => {
-    const execute = [product.returnCreate];
-    sinon.stub(connection, 'execute').resolves(execute);
+    const execute = [product.returnGet];
+    sinon.stub(connection, "execute").resolves(execute);
   });
   after(async () => {
     connection.execute.restore();
   });
-  describe('when succeed', async () => {
-    it('returns an object array', async () => {
+  describe("when succeed", async () => {
+    it("must not be empty", async () => {
       const GetProductList = await productModel.getProductList();
 
-      expect(GetProductList).to.have.keys('id', 'name', 'quantity')
+      expect(GetProductList).to.not.be.empty;
+    });
+    it("return an array", async () => {
+      const GetProductList = await productModel.getProductList();
+
+      expect(GetProductList).to.be.an("array");
+    });
+    it("have keys", async () => {
+      const GetProductList = await productModel.getProductList();
+
+      expect(GetProductList[0]).to.have.keys("id", "name", "quantity");
     });
   });
 });
 
-describe('Create a new product', () => {
+describe("Create a new product", () => {
   before(async () => {
     const execute = [{ insertId: 1 }];
-    sinon.stub(connection, 'execute').resolves(execute);
+    sinon.stub(connection, "execute").resolves(execute);
   });
   after(async () => {
     connection.execute.restore();
   });
-  describe('when succeed', () => { 
-    it('returns the new product object', async () => {
+  describe("when succeed", () => {
+    it("must not be empty", async () => {
       const { name, quantity } = product.correct;
-      const response = await productModel.create({name, quantity});
+      const response = await productModel.create({ name, quantity });
 
-      expect(response).to.have.keys('id', 'name', 'quantity')
+      expect(response).to.not.be.empty;
+    });
+    it("returns the new product object", async () => {
+      const { name, quantity } = product.correct;
+      const response = await productModel.create({ name, quantity });
+
+      expect(response).to.be.an("object");
+    });
+    it("have keys", async () => {
+      const { name, quantity } = product.correct;
+      const response = await productModel.create({ name, quantity });
+
+      expect(response).to.have.keys("id", "name", "quantity");
     });
   });
 });
 
-describe('Update a product', () => {
+describe("Update a product", () => {
   before(async () => {
     const execute = [{ insertId: 1 }];
-    sinon.stub(connection, 'execute').resolves(execute);
+    sinon.stub(connection, "execute").resolves(execute);
   });
   after(async () => {
     connection.execute.restore();
   });
-  describe('when succeed', () => { 
-    it('return the updated product', async () => {
+  describe("when succeed", () => {
+    it("return the updated product", async () => {
       const response = await productModel.update(product.returnUptade);
 
-      expect(response).to.have.keys('id', 'name', 'quantity')
+      expect(response).to.have.keys("id", "name", "quantity");
     });
   });
 });
 
-describe('Remove a product', () => {
+describe("Remove a product", () => {
   before(async () => {
     const execute = [{ insertId: 1 }];
-    sinon.stub(connection, 'execute').resolves(execute);
+    sinon.stub(connection, "execute").resolves(execute);
   });
   after(async () => {
     connection.execute.restore();
   });
-  describe('when succeed', () => { 
-    it('returns void', async () => {
+  describe("when succeed", () => {
+    it("returns void", async () => {
       const response = await productModel.remove(product.returnUptade);
 
       expect(response).to.be.equal();
