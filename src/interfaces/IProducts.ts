be09725583984938d79e82110470236eac372f8e
João Reis <joaoreisjpk@gmail.com>
2022-02-02 17:34:01 -0300

    adding the products interface

---
+++ src/interfaces/IProducts.ts
@@ -0,0 +1,19 @@
+interface basicProduct {
+  name: string;
+  quantity: number;
+}
+
+/* interface noNameProduct {
+  quantity: number
+}
+
+interface noQuantityProduct {
+  name: string;
+} */
+
+interface basicProductResponse {
+  id: number;
+  name: string;
+  quantity: number;
+}
+
