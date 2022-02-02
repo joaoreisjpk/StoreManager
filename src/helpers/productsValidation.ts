dd3d37b49ab8facc4118cabbb5e85323ef4f12c6
João Reis <joaoreisjpk@gmail.com>
2022-02-02 18:08:40 -0300

    adding the producsValidation types

--- src/helpers/productsValidation.ts
+++ src/helpers/productsValidation.ts
@@ -1,11 +1,11 @@
-function NameValidation(name) {
+function NameValidation(name: string) {
   if (!name) return { code: 400, message: '"name" is required' };
   if (name.length < 5) {
     return { code: 422, message: '"name" length must be at least 5 characters long' };
   }
 }
 
-function QuantityValidation(quantity) {
+function QuantityValidation(quantity: number) {
   if (!quantity && quantity !== 0) return { code: 400, message: '"quantity" is required' };
   if (typeof quantity !== 'number' || quantity < 1) {
     return { code: 422, message: '"quantity" must be a number larger than or equal to 1' };
